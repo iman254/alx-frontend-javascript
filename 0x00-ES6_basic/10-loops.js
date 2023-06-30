f53d7a210ac4002d9971423dd15b852a941f2cec
iman254 <stephaniemarquinz@icloud.com>
2023-06-30 00:51:04 +0000

    report object

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const final = [];
-    for (const value of array) {
-      final.push(appendString + value);
-    }
+  const final = [];
+  for (const value of array) {
+    final.push(appendString + value);
+  }
 
-    return final;
+  return final;
 }
