6cc87c7ac4aa9e6c1b3e733151fcaf8cebf5c5db
iman254 <stephaniemarquinz@icloud.com>
2023-07-05 12:10:14 +0000

    simple promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve()
-		}else {
-			reject()
-		}
-	})
+  return new Promise((resolve, reject) => {
+    if (success) {
+	resolve()
+    }else {
+	reject()
+    }
+  })
 }
