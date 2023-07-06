172eea802552273d67499972b04d9082e6f50ab9
iman254 <stephaniemarquinz@icloud.com>
2023-07-06 15:10:38 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const profile = uploadPhoto();
-  const user = createUser;
+  const user = createUser();
   return Promise.all([profile, user])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
