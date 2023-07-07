e79b92911f6c4055fefcf40ffc24e4ea1d9d5a20
iman254 <stephaniemarquinz@icloud.com>
2023-07-07 11:47:05 +0000

    task 8'

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,11 @@
+export default function divideFunction(numerator, denominator) {
+  try{
+    const division = numerator / denominator;
+    if (denominator === 0) {
+      throw new Error();
+    }
+    return division;
+  } catch (err) {
+    throw Error('cannot divide by 0');
+  }
+}
